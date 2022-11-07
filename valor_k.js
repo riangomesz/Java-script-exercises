cb901ffecd56b5d1f1d00a18d955562eefb3d4f8
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-11-07 10:17:14 -0300

    Add files via upload

---
+++ valor_k.js
@@ -0,0 +1,35 @@
+function calculaTopOcorrenciasDeQueries(texto, queries, k) {
+    let queriesComIdx = [];
+    for (let cdQ of queries) {
+        let indice = texto.indexOf(cdQ);
+        let count = (texto.match(new RegExp(cdQ, 'g')) || []).length;
+        if (indice != -1) {
+            queriesComIdx.push({ 'term': cdQ, 'idx': texto.indexOf(cdQ), 'count': count });
+        }
+    }
+    /* Ordenar o aray de objetos */
+    queriesComIdx.sort((a, b) => {
+        if (b.count == a.count) {
+            if (a.idx > b.idx) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+        if (a.count < b.count) {
+            return 1;
+        }
+        if (a.count > b.count) {
+            return -1;
+        }
+        return 0;
+    });
+
+    arrReturn = queriesComIdx.map((cdItem) => {
+        return cdItem['term'];
+    });
+
+    return arrReturn.slice(0, k);
+}
+
+module.exports = calculaTopOcorrenciasDeQueries
