b73768e2373d34d394452e2c337b5b5da57c4d56
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-11-09 13:11:12 -0300

    Update live_sorteio.js

--- live_sorteio.js
+++ live_sorteio.js
@@ -1,3 +1,17 @@
+/*
+A chance de uma pessoa ser sorteada é diretamente proporcional às horas de conteúdo que ela assistiu naquele canal. 
+Assim sendo, uma pessoa que ficou 20 horas acompanhando esse streamer tem o dobro de chances de ser sorteado do que uma pessoa que o assistiu durante 10 horas, 
+se ambas não forem assinantes pagos. O número de horas é recebido em minutos e é sempre arredondado para cima, ou seja:
+
+  181 minutos = 4 horas
+  180 minutos = 3 horas
+  
+Se você possui uma assinatura paga da plataforma, suas chances são dobradas. Assim, no caso acima, se a pessoa que assistiu 10 horas for 
+assinante premium e a que assistiu 20 horas não, ambos terão a mesma chance no sorteio
+
+Assuma que você vai receber os dados de horas assistidas e de assinaturas em dois vetores, no modelo mostrado abaixo:
+
+
 function calculaPorcentagemSorteio(assinante,minutosAssistidos) {
   
     let numHorasInit = minutosAssistidos.map((cdTime, idx) => {
