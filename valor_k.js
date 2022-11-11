5b1b6144c97111c0dd13a4518f9cf4cb02e2de01
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-11-11 13:07:42 -0300

    Add files via upload

--- valor_k.js
+++ valor_k.js
@@ -1,21 +1,3 @@
-/* 
-Dada um texto qualquer e um lista de termos de pesquisa (sequencia de caracteres), retorne os primeiros
-K termos mais recorrentes na string, onde K é um parâmetro configurável. 
-
-Exemplo:
-String: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
-Lista de termos: ["a", "em", "i", "el"]
-K: 2
-Resultado: ["i", "a"]
-Explicação:
-
-Ocorrências de cada termo,"i": 11, "a": 7, "em": 2, "el": 1, com K = 2, retornamos "i" e "a" ordenados conforme a quantidade de ocorrências de cada termo.
-
-Obs: Quando houver termos com quantidades iguais, priorizar o retorno de acordo com a ordem de ocorrência do termo na string.
-*/
-
-
-
 function calculaTopOcorrenciasDeQueries(texto, queries, k) {
     let queriesComIdx = [];
     for (let cdQ of queries) {
