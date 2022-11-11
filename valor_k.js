6a957bad80923c7f17fc8b1bb83bd00d4ba71dc0
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2022-11-11 13:09:50 -0300

    Update valor_k.js

--- valor_k.js
+++ valor_k.js
@@ -1,5 +1,4 @@
 /* 
-
 Dada um texto qualquer e um lista de termos de pesquisa (sequencia de caracteres), retorne os primeiros K termos mais recorrentes na string, onde K é um parâmetro configurável.
 Exemplo:
 String: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
@@ -7,7 +6,8 @@ Lista de termos: ["a", "em", "i", "el"]
 K: 2
 Resultado: ["i", "a"]
 Explicação:
-
+Ocorrências de cada termo,"i": 11, "a": 7, "em": 2, "el": 1, com K = 2, retornamos "i" e "a" ordenados conforme a quantidade de ocorrências de cada termo.
+Obs: Quando houver termos com quantidades iguais, priorizar o retorno de acordo com a ordem de ocorrência do termo na string.
 */
 
 
